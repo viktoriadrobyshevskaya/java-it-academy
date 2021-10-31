3d79f120d07f98c6392d6edc17903e1b8da9dd98
Виктория Дробышевская <75070961+viktoriadrobyshevskaya@users.noreply.github.com>
2021-10-31 17:51:28 +0300

    Delete hw/hw4_new directory

--- hw/hw4_new/factories/CreateCar.java
+++
@@ -1,7 +0,0 @@
-package by.it_academy.hw.hw4_new.factories;
-
-import by.it_academy.hw.hw4_new.enums.*;
-
-public interface CreateCar<T> {
-    public <S> T createCar(Color color, int year, WheelSize wheelSize, Capacity engineCapacity, S type);
-}
--- hw/hw4_new/factories/SearchInStorage.java
+++
@@ -1,7 +0,0 @@
-package by.it_academy.hw.hw4_new.factories;
-
-import by.it_academy.hw.hw4_new.enums.*;
-
-public interface SearchInStorage<T> {
-    public <S> T searchInStorage(Color color, int year, WheelSize wheelSize, Capacity engineCapacity, S type);
-}
